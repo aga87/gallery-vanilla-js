ab9b566c43abb9bfae7133b04d067391b9c690d1
Aga <aga.labonarska@outlook.com>
2020-08-04 15:50:11 +0100

    sw

---
+++ sw.js
@@ -0,0 +1,46 @@
+const cacheName = 'v1';
+
+// Install
+
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
+});
+
+// Activate
+
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker: Activated');
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+
+self.addEventListener('fetch', (e) => {
+  console.log('Service Worker: Fetching');
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        // Make copy/clone of response
+        const resClone = res.clone();
+        // Open cache
+        caches.open(cacheName).then((cache) => {
+          // Add the response to cache
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch((err) => caches.match(e.request).then((res) => res))
+  );
+});
